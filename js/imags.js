47b4e83f5f31507b4cb6c0d9d7fab8c92537a7b7
tomorrw2 <152231749+tomorrw2@users.noreply.github.com>
2023-11-28 03:09:28 +0300

    Add files via upload

---
+++ js/imags.js
@@ -0,0 +1,23 @@
+				$(function() {
+				$('img').mouseover(function() {
+				$(this).fadeTo('400' , 0.50);
+				$(this).fadeTo('400' , 1);
+				});
+				});
+
+				$(function() {
+				$('img.luay2011').mouseover(function() {
+
+				$(this).animate({rotate: '40deg'});
+				$(this).animate({rotate: '-=40deg'});
+
+				});
+				});
+				$(function() {
+				$('img.luay2012').mouseover(function() {
+
+				$(this).animate({rotate: '40deg'});
+				$(this).animate({rotate: '-=40deg'});
+
+				});
+				});
